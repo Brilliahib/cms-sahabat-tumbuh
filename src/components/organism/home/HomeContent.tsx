4c729eab2a9449737c13a79ee7b330d515735a58
brilliahib <brilliahib21@gmail.com>
2024-09-29 00:56:37 +0700

    fix: layout on landing page

--- src/components/organism/home/HomeContent.tsx
+++ src/components/organism/home/HomeContent.tsx
@@ -1,13 +1,3 @@
-import HomeArticle from "@/components/molecules/home/HomeArticle";
-import HomeGames from "@/components/molecules/home/HomeGames";
-
 export default function HomeContent() {
-  return (
-    <>
-      <div className="md:space-y-12 space-y-8">
-        <HomeGames />
-        <HomeArticle />
-      </div>
-    </>
-  );
+  return <></>;
 }
