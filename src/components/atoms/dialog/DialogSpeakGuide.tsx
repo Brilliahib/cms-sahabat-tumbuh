d6e4574835bf2e26241abe287670a6d570d18554
brilliahib <brilliahib21@gmail.com>
2024-10-15 15:20:30 +0700

    fix: error deployment issue on dialog speak guide

--- src/components/atoms/dialog/DialogSpeakGuide.tsx
+++ src/components/atoms/dialog/DialogSpeakGuide.tsx
@@ -15,37 +15,35 @@ export default function DialogSpeakGuide({
   setOpen,
 }: DialogCalculationGuideProps) {
   return (
-    <>
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Panduan Penggunaan</DialogTitle>
-          </DialogHeader>
-          <div className="space-y-4 text-gray-700">
-            <p>
-              Fitur ini memungkinkan Anda untuk mendengar pengucapan huruf-huruf
-              alfabet dengan mudah. Berikut adalah langkah-langkah untuk
-              menggunakannya:
-            </p>
-            <ol className="list-decimal list-inside space-y-2">
-              <li>
-                <strong>Ketik Kalimat:</strong> Gunakan kolom input untuk
-                mengetikkan kalimat yang ingin Anda dengar.
-              </li>
-              <li>
-                <strong>Ucapkan Kalimat:</strong> Setelah mengetik kalimat, klik
-                tombol "Ucapkan" untuk mendengar pengucapan kalimat tersebut.
-              </li>
-              <li>
-                <strong>Klik Huruf:</strong> Anda juga dapat langsung
-                mendengarkan pengucapan huruf dengan mengklik huruf yang
-                tersedia.
-              </li>
-            </ol>
-            <p>Selamat mencoba!</p>
-          </div>
-        </DialogContent>
-      </Dialog>
-    </>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Panduan Penggunaan</DialogTitle>
+        </DialogHeader>
+        <div className="space-y-4 text-gray-700">
+          <p>
+            Fitur ini memungkinkan Anda untuk mendengar pengucapan huruf-huruf
+            alfabet dengan mudah. Berikut adalah langkah-langkah untuk
+            menggunakannya:
+          </p>
+          <ol className="list-decimal list-inside space-y-2">
+            <li>
+              <strong>Ketik Kalimat:</strong> Gunakan kolom input untuk
+              mengetikkan kalimat yang ingin Anda dengar.
+            </li>
+            <li>
+              <strong>Ucapkan Kalimat:</strong> Setelah mengetik kalimat, klik
+              tombol &quot;Ucapkan&quot; untuk mendengar pengucapan kalimat
+              tersebut.
+            </li>
+            <li>
+              <strong>Klik Huruf:</strong> Anda juga dapat langsung mendengarkan
+              pengucapan huruf dengan mengklik huruf yang tersedia.
+            </li>
+          </ol>
+          <p>Selamat mencoba!</p>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
