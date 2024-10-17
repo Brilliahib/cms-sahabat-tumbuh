67b5b0a2af8f1306a8423a5355016391af79bded
brilliahib <brilliahib21@gmail.com>
2024-10-17 19:29:46 +0700

    fix: change font to poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Paytone_One, Plus_Jakarta_Sans } from "next/font/google";
+import { Paytone_One, Plus_Jakarta_Sans, Poppins } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/atoms/Navbar";
-import NavbarDesktop from "@/components/organism/navbar/Navbar";
 import GlobalProvider from "@/components/organism/GlobalProvider";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -22,6 +20,13 @@ const jakartaSans = Plus_Jakarta_Sans({
   variable: "--font-jakartasans",
 });
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-jakartasans",
+});
+
 const paytoneOne = Paytone_One({
   subsets: ["latin"],
   display: "swap",
@@ -37,11 +42,11 @@ export default function RootLayout({
   return (
     <html
       lang="id"
-      className={`${jakartaSans.variable} ${paytoneOne.variable}  antialiased`}
+      className={`${jakartaSans.variable} ${paytoneOne.variable} ${poppins.variable}  antialiased`}
     >
       <body>
         <GlobalProvider>
-          <main className="font-jakartasans">{children}</main>
+          <main className="font-poppins">{children}</main>
           <Toaster />
         </GlobalProvider>
       </body>
